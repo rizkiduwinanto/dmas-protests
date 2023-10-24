222e9c29b8896e53b684183b1920b97299f38e88
UnknownGamerYT <kyriakosantoniou3@gmail.com>
2023-10-24 17:21:22 +0200

    BLM simulation and event chain + agent amount variable instead of  constant

--- agents.py
+++ agents.py
@@ -53,6 +53,14 @@ class Agent:
         :return: Affected dissatisfaction level of the agent.
         """
         return self.affected_dissatisfaction
+
+    def set_affected_dissatisfaction(self,dissatisfaction):
+        """
+        Get the agent's affected dissatisfaction level.
+
+        :return: Affected dissatisfaction level of the agent.
+        """
+        self.affected_dissatisfaction = dissatisfaction
     
     def get_close_friends(self):
         return self.close_friends
@@ -60,10 +68,28 @@ class Agent:
         return self.friends
     def get_network(self):
         return self.network
+    
+    def cap(self):
+        # cap currently not used outside BLM
+        if self.affected_dissatisfaction>100:
+            self.affected_dissatisfaction = 100
+        if self.affected_dissatisfaction<0:
+            self.affected_dissatisfaction = 0
+
 
     def event_update_affected_dissatisfaction(self,amount):
-        self.affected_dissatisfaction += amount
-
+        self.affected_dissatisfaction += amount[0]
+        self.cap()
+        for close_friend in self.close_friends:
+            close_friend.set_affected_dissatisfaction(close_friend.get_affected_dissatisfaction() + amount[1])
+            close_friend.cap()
+        for friend in self.friends:
+            friend.set_affected_dissatisfaction(friend.get_affected_dissatisfaction() + amount[2])
+            friend.cap()
+        for net_agent in self.network:
+            net_agent.set_affected_dissatisfaction(net_agent.get_affected_dissatisfaction() + amount[3])
+            net_agent.cap()
+    
     def add_close_friend(self, close_friend):
         """
         Add a close friend to the agent's list of close friends.
@@ -181,12 +207,98 @@ def per_group(dissatisfaction):
         affect[4] += (rnd.randint(-1, 1) * PUBLIC_AF)
     return affect
 
-def BLM(tick,length):
-    return None
+def BLM_simulation(agents):
+    """
+    Simulate the evolution of agent dissatisfaction over time.
+
+    Args:
+    agents (np.array): An array of Agent objects.
+
+    Returns:
+    list: A list of dissatisfaction values for each iteration.
+    """
+    iterations = 3285 # 9 years
+    dissatisfaction = []
+
+    for tick in range(iterations):
+        if tick%365 == 0 :
+            print(tick/365 + 2013,"year")
+        dissatisfaction_update = BLM_even_list(tick)
+        if dissatisfaction_update != [0, 0, 0, 0, 0]:
+            rng_agent = agents[rnd.randint(0, agents.size-1)]
+            rng_agent.event_update_affected_dissatisfaction(dissatisfaction_update)
+            for agent in agents:
+                agent.set_affected_dissatisfaction(agent.get_affected_dissatisfaction() + dissatisfaction_update[4])
+            print(dissatisfaction_update, " ",tick)
+
+        dissatisfaction.append(average_total_dissatisfaction(agents))
+        for agent in agents:
+            agent.update_dissatisfaction()
+
+    return dissatisfaction
+
+def BLM_even_list(tick):
+    dissatisfaction = [0,0,0,0,0]
+    if tick<365:              #2013-2014
+        if 0 == rnd.randint(0,30): # 12 deaths 
+            dissatisfaction = [100,100,70,10,1]
+        if tick == 240: # one 500 african american protest
+            dissatisfaction[4] += 1
+
+    if tick>=365 and tick<730:#2014-2015
+        if 0 == rnd.randint(0,24): # 15 deaths 
+            dissatisfaction = [100,100,70,10,1]
+        if 0 == rnd.randint(0,36): # 10 protests 
+            dissatisfaction[4] += 1
+
+    if tick>=730 and tick<1095:#2015-2016
+        if 0 == rnd.randint(0,33): # 15 deaths 
+            dissatisfaction = [100,100,70,10,1]
+        if tick == 760: # hundreads of protests in january 
+            dissatisfaction[4] += 15
+     
+    if tick>=1095 and tick<1460:#2016-2017
+        if 0 == rnd.randint(0,365): # 1 death 
+            dissatisfaction = [100,100,70,10,1]
+        if tick == 1200: # month long BLM art exhibition 
+            dissatisfaction[4] += 10
+        if tick == 1300: # protest
+            dissatisfaction[4] += 1
+    if tick>=1460 and tick<1825:#2017-2018
+        if 0 == rnd.randint(0,182): # 2 deaths 
+            dissatisfaction = [100,100,70,10,1]
+        if tick == 1700: # BLM fb page scam
+            dissatisfaction[4] -= 5
+
+    if tick>=1825 and tick<2190: #2018-2019 no data  
+        None
+    
+    if tick>=2190 and tick<2555: #2019-2020
+        if 0 == rnd.randint(0,182): # 2 deaths 
+            dissatisfaction = [100,100,70,10,1]
+        if tick == 2330: # George Floyd protests (450 major protests)
+            dissatisfaction[4] += 40
+        if tick == 2400: # Goverment intrvention
+            dissatisfaction[4] -= 60
+
+    if tick>=2555: #2020-2023
+         if 0 == rnd.randint(0,182): # court cases settlements about BLM deaths
+            dissatisfaction = [-100,-100,-70,-10,-1]
+    return dissatisfaction
 
 
 
+def create_agents_0(amount):
+    """
+    Create an array of Agent objects.
 
+    Returns:
+    np.array: An array of Agent objects.
+    """
+    agents = np.array([])
+    for i in range(amount):
+        agents = np.append(agents, Agent(0, i))
+    return agents
 
 def create_agents():
     """
@@ -208,7 +320,7 @@ def create_close_friendships(agents):
     Args:
     agents (np.array): An array of Agent objects.
     """
-    for i in range(25):
+    for i in range(int(agents.size/4)):
         close_friendgroup = np.array([agents[0 + i * 4], agents[1 + i * 4], agents[2 + i * 4], agents[3 + i * 4]])
         for j in range(4):
             agents[(j + i * 4)].add_close_friend(np.array([agents[(k + i * 4)] for k in range(4) if k != j]))
@@ -221,7 +333,7 @@ def create_friendships(agents):
     Args:
     agents (np.array): An array of Agent objects.
     """
-    for i in range(100):
+    for i in range(agents.size):
         while len(agents[i].friends) != NUMBER_OF_FRIENDS:
             current_agent = agents[rnd.randint(0, 99)]
             if not (np.any(agents[i].close_friends == current_agent)) and not (
@@ -236,9 +348,9 @@ def create_networks(agents):
     Args:
     agents (np.array): An array of Agent objects.
     """
-    for i in range(100):
+    for i in range(agents.size):
         while len(agents[i].network) != NUMBER_OF_SOCIAL_NETWORK_FRIENDS:
-            current_agent = agents[rnd.randint(0, 99)]
+            current_agent = agents[rnd.randint(0, agents.size-1)]
             if not (np.any(agents[i].close_friends == current_agent)) and not (
                     np.any(agents[i].friends == current_agent) and not (np.any(agents[i].network == current_agent))):
                 agents[i].network = np.append(agents[i].network, current_agent)
@@ -315,7 +427,7 @@ def plot_dissatisfaction(dissatisfaction):
 
 
 def main():
-    agents = create_agents()
+    agents = create_agents_0(1000)
     create_close_friendships(agents)
     create_friendships(agents)
     create_networks(agents)
@@ -324,7 +436,7 @@ def main():
     print_agent_information(agents[0])
     print(average_total_dissatisfaction(agents))
 
-    dissatisfaction = run_simulation(agents)
+    dissatisfaction = BLM_simulation(agents)
 
     # Print information about the first agent again after running the simulation
     print_agent_information(agents[0])
